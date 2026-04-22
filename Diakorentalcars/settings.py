8d7a71a1b5a923f1a3432e233178716620b9b35f
Nambale Kelly <kellyjnambale@gmail.com>
2026-04-22 15:02:28 +0300

    Initial commit

--- Diakorentalcars/settings.py
+++ Diakorentalcars/settings.py
@@ -209,10 +209,13 @@ ACCOUNT_SIGNUP_FIELDS = ['email*']
 
 SOCIALACCOUNT_LOGIN_ON_GET = True
 SOCIALACCOUNT_AUTO_SIGNUP = True
+SOCIALACCOUNT_EMAIL_AUTHENTICATION = True
+SOCIALACCOUNT_EMAIL_AUTHENTICATION_AUTO_CONNECT = True
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'SCOPE': ['profile', 'email'],
         'AUTH_PARAMS': {'access_type': 'online'},
+        'VERIFIED_EMAIL': True,
         'APP': {
             'client_id': os.getenv('GOOGLE_CLIENT_ID', ''),
             'secret': os.getenv('GOOGLE_CLIENT_SECRET', ''),
